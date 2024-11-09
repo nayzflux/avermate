0aa6a0189751c55548fc974a0f208b1ce516672c
nayzflux <55506519+nayzflux@users.noreply.github.com>
2024-11-09 06:42:42 +0100

    feat: session middleware

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,15 @@
+import { auth, type Session, type User } from "@/lib/auth";
 import { env } from "@/lib/env";
 import authRoutes from "@/routes/auth";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
-const app = new Hono().basePath("/api");
+const app = new Hono<{
+  Variables: {
+    user: User | null;
+    session: Session | null;
+  };
+}>().basePath("/api");
 
 // CORS
 app.use(
@@ -13,6 +19,21 @@ app.use(
     credentials: true,
   })
 );
+
+// Session Middleware
+app.use("*", async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    return next();
+  }
+
+  c.set("user", session.user);
+  c.set("session", session.session);
+  return next();
+});
 
 app.route("/auth", authRoutes);
 
