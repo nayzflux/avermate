f2e3d52af04fd021399b78e94fcaf431112cc9b7
nayzflux <55506519+nayzflux@users.noreply.github.com>
2024-12-11 21:16:50 +0100

    docker api

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   casing: "snake_case",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN!,
   },
 });
--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,11 +1,12 @@
 import * as schema from "@/db/schema";
 import { env } from "@/lib/env";
-import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client/web";
+import { drizzle } from "drizzle-orm/libsql/web";
 
 // Create libsql client
 const client = createClient({
   url: env.DATABASE_URL,
+  authToken: env.DATABASE_AUTH_TOKEN,
 });
 
 // Create drizzle client
