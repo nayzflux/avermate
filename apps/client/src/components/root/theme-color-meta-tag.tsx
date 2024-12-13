0bb0b97a1308d3846022645d33c0154442acb5f4
Fefedu973 <80718477+Fefedu973@users.noreply.github.com>
2024-12-13 19:03:22 +0100

    theme

--- apps/client/src/components/root/theme-color-meta-tag.tsx
+++ apps/client/src/components/root/theme-color-meta-tag.tsx
@@ -1,30 +1,13 @@
 "use client";
-
-import { useEffect } from "react";
+import { useTheme } from "next-themes";
 
 export default function ThemeColorMetaTag() {
-  useEffect(() => {
-    // Safely access `document` in the browser
-    const metaTag = document.getElementById("themeColor") as HTMLMetaElement;
-    if (!metaTag) return;
-
-    const isDarkMode = document.documentElement.classList.contains("dark");
-    metaTag.content = isDarkMode ? "#09090b" : "#ffffff";
-  }, []);
-
-  useEffect(() => {
-    if ("virtualKeyboard" in navigator) {
-      // Opt into overlay mode so the viewport doesn’t resize automatically
-      (navigator as any).virtualKeyboard.overlaysContent = true;
-    }
-  }, []);
-
-  // SSR sees just a default meta
+  const theme = useTheme();
   return (
     <meta
       id="themeColor"
       name="theme-color"
-      content="#09090b" // Default for SSR
+      content={theme.resolvedTheme === "dark" ? "#09090b" : "#ffffff"}
     />
   );
 }
