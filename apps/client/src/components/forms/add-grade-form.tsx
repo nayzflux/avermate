4389493787da8775134f953396f4a1cff5484820
Fefedu973 <80718477+Fefedu973@users.noreply.github.com>
2025-01-07 18:15:49 +0100

    Update add-grade-form.tsx

--- apps/client/src/components/forms/add-grade-form.tsx
+++ apps/client/src/components/forms/add-grade-form.tsx
@@ -340,6 +340,9 @@ export const AddGradeForm = ({
                       onSelect={(date) => {
                         console.log(date);
                         field.onChange(date);
+                        console.log(
+                          formatDates.formatLong(new Date(field.value))
+                        );
                         setIsManualPeriod(false); // Reset manual period selection
                       }}
                       disabled={(date) =>
