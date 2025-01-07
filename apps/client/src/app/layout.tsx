37590001ac1fe4f2bcb5fb9cf86279d7bafe2791
nayzflux <55506519+nayzflux@users.noreply.github.com>
2025-01-07 19:27:36 +0100

    feat: umami analytics

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -2,14 +2,15 @@ import QueryProvider from "@/providers/query-provider";
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { ThemeColorMetaTag } from "@/components/color";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 import ThemeProvider from "@/providers/theme-provider";
-import { Gabarito } from "next/font/google";
-import { ThemeColorMetaTag } from "@/components/color";
 import { Viewport } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
+import { Gabarito } from "next/font/google";
+import Script from "next/script";
 
 const gabarito = Gabarito({
   subsets: ["latin"],
@@ -43,6 +44,11 @@ export default async function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.webmanifest" />
         <meta name="theme-color" content="#09090b" />
+        <Script
+          defer
+          src="https://umami.avermate.fr/script.js"
+          data-website-id="0911750a-9051-4ad2-9296-95acd91b78a4"
+        />
       </head>
       <body className={cn("", gabarito.className)}>
         <QueryProvider>
