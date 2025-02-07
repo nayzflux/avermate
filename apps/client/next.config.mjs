0077bc77b953f11d95174937ef1ca848827b1fae
Fefedu973 <80718477+Fefedu973@users.noreply.github.com>
2025-02-07 19:36:43 +0100

    Revert "test"
    This reverts commit c65907a38062b47dfcfb081abb3c8e407d46a5e6.

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -13,23 +13,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    // This ensures recharts alpha works in production
-    config.resolve = {
-      ...config.resolve,
-      fallback: {
-        ...config.resolve.fallback,
-        util: false,
-        crypto: false,
-        assert: false,
-        stream: false,
-        fs: false,
-        os: false,
-        path: false,
-      },
-    };
-    return config;
-  },
 };
 
 export default withNextIntl(nextConfig);
